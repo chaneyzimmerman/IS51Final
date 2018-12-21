f15432401a132dd7bc3a7fffbbc67c475702a53a
Chaney Zimmerman <chaneyzimmerman@Chaneys-MacBook-Air.local>
2018-12-20 19:47:05 -0800

    exam end

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastService } from '../toast/toast.service';
+import { LocalStorageService } from '../localStorageService';
 import { Subject } from 'rxjs';
+
+export interface IUser {
+  id?: number;
+  username: string;
+  password: string;
+}
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,11 +17,32 @@ import { Subject } from 'rxjs';
 })
 export class LoginComponent implements OnInit {
 
+  user: IUser = { username: '', password: '' };
+  localStorageService: LocalStorageService<IUser>;
+  currentUser: IUser = null;
   constructor(private router: Router, private toastService: ToastService) {
+    this.localStorageService = new LocalStorageService('user');
   }
 
   ngOnInit() {
+    this.currentUser = this.localStorageService.getItemsFromLocalStorage();
+    if(this.currentUser != null) {
+      this.router.navigate(['home']);
+    }
+  }
 
+  login(user: IUser) {
+    const defaultUser: IUser = { username: 'czimmerman', password: 'chaney123' };
+    if (user.username !== '' && user.password !== '') {
+      if (user.username === defaultUser.username && user.password === defaultUser.password) {
+        this.localStorageService.saveItemsToLocalStorage(user);
+        this.router.navigate(['cart', user]);
+      } else {
+        this.toastService.showToast('danger', 15000, 'Invalid Credentials!');
+      }
+    } else {
+      this.toastService.showToast('danger', 15000,  'Login failed! Please specify username and password');
+    }
   }
 
 }
--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -12,6 +12,11 @@ export class MainNavComponent implements OnInit {
   constructor(private router: Router, private toastService: ToastService) { }
 
   ngOnInit() {
+    
+  }
+
+  showAbout() {
+    this.toastService.showToast('success', 5000, 'This application is designed by Chaney Zimmerman. (C) 2018');
   }
 
 }
